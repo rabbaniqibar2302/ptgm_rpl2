8811e6723e8c3815f846e27c57704d9af6bc11e1
rabbaniqibar2302 <rizkiakbar2302@gmail.com>
2023-10-11 14:26:46 +0700

    ptgm edit fix

--- CRUD NATIVE/proses_edit.php
+++ CRUD NATIVE/proses_edit.php
@@ -8,6 +8,23 @@ $email = $_POST['email'];
 $no_hape = $_POST['no_hape'];
 
 // perintah untuk merubah database sesuai dengan id
-mysqli_query($host,"update user set nama='$nama', email='$email', no_hape='$no_hape' where id='$id' ");
+$edit = mysqli_query($host,"update user set nama='$nama', email='$email', no_hape='$no_hape' where id='$id' ");
 
+// notifikasi
+// yang dimaksud hanya menulis variabel adalah true
+if($edit){
+    echo"
+    <script>
+    alert('Data Berhasil Di Update');
+    window.location.href = 'index.php';
+    </script>
+    ";
+}else{
+    echo"
+    <script>
+    alert('Data Tidak Berhasil Di Update');
+    window.location.href = 'edit.php';
+    </script>
+    ";
+}
 ?>
