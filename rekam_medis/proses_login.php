f43f40f6b994ca5d4c611a58ed71555e67f7f662
rabbaniqibar2302 <rizkiakbar2302@gmail.com>
2023-11-16 11:25:27 +0700

    aku ga pindah, cuman resign

--- rekam_medis/proses_login.php
+++ rekam_medis/proses_login.php
@@ -6,5 +6,34 @@ $nama = $_POST['username'];
 $pass = $_POST['password'];
 
 // Perintah / Query
+// Query untuk dokter
+$dokter = mysqli_query($host,"select * from user where role = 'dokter' AND username = '$nama' ");
+
+// Query untuk pasien
+$pasien = mysqli_query($host,"select * from user where role = 'pasien' AND username = '$nama' ");
+
+// Keluarkan datanya pakai porich
+foreach($dokter as $data){
+    $user = $data['role'];
+}
+foreach($pasien as $data){
+    $user = $data['role'];
+}
+
+// Seleksi dan arahkan ke halamnnya 
+
+if($user == "dokter"){
+    echo"
+    <script>
+    window.location.href = 'dokter/index.php';
+    </script>
+    ";
+}elseif($user == "pasien"){
+    echo"
+    <script>
+    window.location.href = 'pasien/index.php';
+    </script>
+    ";
+}
 
 ?>
